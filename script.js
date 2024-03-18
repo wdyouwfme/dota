c16bcfff03b4431a0964454398ac8763104c88c7
Anastasia <anastasiy12342@yandex.ru>
2024-03-18 21:05:32 +0300

    /

--- script.js
+++ script.js
@@ -1,171 +1,72 @@
 document.addEventListener('DOMContentLoaded', function () {
     const gridContainer = document.querySelector('.grid-container');
-    const selectedImageContainerEnemy = document.getElementById('enemyImageContainer');
-    gridContainer.addEventListener('click', function (event) {
-        if (event.target.tagName === 'IMG') {
-            event.target.classList.toggle('grid-itemimg-hover');
-            const selectedImagesEnemy = selectedImageContainerEnemy.querySelectorAll('.image-item');
-            if (selectedImagesEnemy.length < 4) {
-                const clonedImage = event.target.cloneNode(true);
-                const imageItem = document.createElement('div');
-                imageItem.classList.add('image-item');
-                imageItem.appendChild(clonedImage);
-                selectedImageContainerEnemy.appendChild(imageItem);
-            } else {
-                console.log(selectedImageContainerEnemy);
-            }
-        }
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const gridContainer = document.querySelector('.grid-container');
     const enemyImageContainer = document.getElementById('enemyImageContainer');
     const myImageContainer = document.getElementById('myImageContainer');
     const enemyImageForm = document.getElementById('enemyImageForm');
     const myImageForm = document.getElementById('myImageForm');
+    let activeTeam = 'myTeam'; // Инициализация активной команды
 
-    const selectedImages = [];
+    let selectedImages = {
+        myTeam: [],
+        enemyTeam: []
+    };
+
     gridContainer.addEventListener('click', function (event) {
-        if (event.target.tagName === 'IMG') {
-
+        if (event.target.tagName === 'IMG' && !event.target.hasAttribute('aria-disabled')) {
             const imageId = event.target.parentNode.id;
-
-            const isSelected = selectedImages.includes(imageId);
-
-            if (!isSelected) {
-                selectedImages.push(imageId);
-                console.log(selectedImages)
-                const clonedImage = event.target.cloneNode(true);
-                const imageItem = document.createElement('div');
-                imageItem.classList.add('image-item');
-                imageItem.appendChild(clonedImage);
-                const team = event.target.closest('.section-inner').querySelector('.choise-text-my')
-                    ? myImageContainer
-                    : enemyImageContainer;
-                team.appendChild(imageItem);
-            } else {
-                const index = selectedImages.indexOf(imageId);
-                if (index !== -1) {
-                    selectedImages.splice(index, 1);
-                }
-                console.log(selectedImages)
-                const selectedImageItem = document.querySelector(`.image-item img[src="${event.target.src}"]`);
-                if (selectedImageItem) {
-                    selectedImageItem.parentNode.remove();
-                }
-            }
+            handleSelection(imageId, event.target);
         }
     });
-    enemyImageForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        console.log('Enemy Team Selected Images:', selectedImages);
-    });
-    myImageForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        console.log('My Team Selected Images:', selectedImages);
-    });
-});
-document.addEventListener('DOMContentLoaded', function () {
-    const gridContainers = document.querySelectorAll('.grid-container');
-    const enemyImageContainer = document.getElementById('enemyImageContainer');
-    const myImageContainer = document.getElementById('myImageContainer');
-    const enemyImageForm = document.getElementById('enemyImageForm');
-    const myImageForm = document.getElementById('myImageForm');
-    const selectedImages = [];
 
-    gridContainers.forEach(function (gridContainer) {
-        gridContainer.addEventListener('click', function (event) {
-            if (event.target.tagName === 'IMG') {
-                const imageId = event.target.parentNode.id;
-                const isSelected = selectedImages.includes(imageId);
+    function handleSelection(imageId, target) {
+        const isSelected = selectedImages[activeTeam].includes(imageId);
 
-                if (!isSelected) {
-                    selectedImages.push(imageId);
-                    console.log(selectedImages)
-                    const clonedImage = event.target.cloneNode(true);
-                    const imageItem = document.createElement('div');
-                    imageItem.classList.add('image-item');
-                    imageItem.appendChild(clonedImage);
-                    const team = event.target.closest('.section-inner').querySelector('.choise-text-my')
-                        ? myImageContainer
-                        : enemyImageContainer;
-
-                    team.appendChild(imageItem);
-                } else {
-                    const index = selectedImages.indexOf(imageId);
-                    if (index !== -1) {
-                        selectedImages.splice(index, 1);
-                    }
-                    console.log(selectedImages)
-                    const selectedImageItem = document.querySelector(`.image-item img[src="${event.target.src}"]`);
-                    if (selectedImageItem) {
-                        selectedImageItem.parentNode.remove();
-                    }
-                }
+        if (!isSelected && selectedImages[activeTeam].length < 4) {
+            selectedImages[activeTeam].push(imageId);
+            console.log(selectedImages[activeTeam]);
+            const clonedImage = target.cloneNode(true);
+            const imageItem = document.createElement('div');
+            imageItem.classList.add('image-item');
+            imageItem.appendChild(clonedImage);
+            activeTeam === 'myTeam' ? myImageContainer.appendChild(imageItem) : enemyImageContainer.appendChild(imageItem);
+        } else if (isSelected) {
+            const index = selectedImages[activeTeam].indexOf(imageId);
+            if (index !== -1) {
+                selectedImages[activeTeam].splice(index, 1);
             }
-        });
-    });
+            console.log(selectedImages[activeTeam]);
+            const selectedImageItem = document.querySelector(`.image-item img[src="${target.src}"]`);
+            if (selectedImageItem) {
+                selectedImageItem.parentNode.remove();
+            }
+        }
+
+        if (selectedImages[activeTeam].length === 4) {
+            switchTeam();
+        }
+    }
+
+    function switchTeam() {
+        activeTeam = activeTeam === 'myTeam' ? 'enemyTeam' : 'myTeam';
+        console.log('Switched to', activeTeam, 'team');
+
+        // Переключение активной формы
+        if (activeTeam === 'myTeam') {
+            myImageForm.style.display = 'block';
+            enemyImageForm.style.display = 'none';
+        } else {
+            myImageForm.style.display = 'none';
+            enemyImageForm.style.display = 'block';
+        }
+    }
+
     enemyImageForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        console.log('Enemy Team Selected Images:', selectedImages);
+        console.log('Enemy Team Selected Images:', selectedImages.enemyTeam);
     });
+
     myImageForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        console.log('My Team Selected Images:', selectedImages);
+        console.log('My Team Selected Images:', selectedImages.myTeam);
     });
 });
-// document.addEventListener('DOMContentLoaded', function () {
-//     const gridContainer = document.querySelector('.grid-container');
-//     const selectedImageContainerEnemy = document.getElementById('enemyImageContainer');
-//     const enemyImageContainer = document.getElementById('enemyImageContainer');
-//     const myImageContainer = document.getElementById('myImageContainer');
-//     const enemyImageForm = document.getElementById('enemyImageForm');
-//     const myImageForm = document.getElementById('myImageForm');
-//     const selectedImages = [];
-
-//     gridContainer.addEventListener('click', function (event) {
-//         if (event.target.tagName === 'IMG') {
-//             const imageId = event.target.parentNode.id;
-//             const isSelected = selectedImages.includes(imageId);
-
-//             if (!isSelected) {
-//                 selectedImages.push(imageId);
-//                 console.log(selectedImages)
-
-//                 const clonedImage = event.target.cloneNode(true);
-//                 const imageItem = document.createElement('div');
-//                 imageItem.classList.add('image-item');
-//                 imageItem.appendChild(clonedImage);
-
-//                 const team = event.target.closest('.section-inner').querySelector('.choise-text-my')
-//                     ? myImageContainer
-//                     : enemyImageContainer;
-
-//                 team.appendChild(imageItem);
-//             } else {
-//                 const index = selectedImages.indexOf(imageId);
-//                 if (index !== -1) {
-//                     selectedImages.splice(index, 1);
-//                 }
-//                 console.log(selectedImages)
-
-//                 const selectedImageItem = document.querySelector(`.image-item img[src="${event.target.src}"]`);
-//                 if (selectedImageItem) {
-//                     selectedImageItem.parentNode.remove();
-//                 }
-//             }
-//         }
-//     });
-
-//     enemyImageForm.addEventListener('submit', function (event) {
-//         event.preventDefault();
-//         console.log('Enemy Team Selected Images:', selectedImages);
-//     });
-
-//     myImageForm.addEventListener('submit', function (event) {
-//         event.preventDefault();
-//         console.log('My Team Selected Images:', selectedImages);
-//     });
-// });
